8da342fdb93ed3d4744992e70cb92b1a2f444542
Yit Chun <me@minamiki.com>
2010-12-07 01:35:05 +0800

    cleaned up dashboard.css added input styles to fb.css updated header-css.php implemented displayQuiz.php and quiz.css

--- webroot/inc/header-css.php
+++ webroot/inc/header-css.php
@@ -1,3 +1,4 @@
 <link href="css/fb.css" rel="stylesheet" type="text/css" />
 <link href="css/statusbar.css" rel="stylesheet" type="text/css" />
 <link href="css/dashboard.css" rel="stylesheet" type="text/css" />
+<link href="css/quiz.css" rel="stylesheet" type="text/css" />
