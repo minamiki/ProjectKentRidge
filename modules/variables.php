391a84aee2984d2e4c7cd3328d2f62a34656c842
Yit Chun <me@minamiki.com>
2011-01-02 16:20:22 +0800

    added global variables

---
+++ modules/variables.php
@@ -0,0 +1,12 @@
+<?php
+//----------------------------------------
+// For global variables
+//----------------------------------------
+
+// The base score for awarding of points
+$BASE_POINT = 1;
+
+// The multiplier amount for each increment
+$MULTIPLIER = 0.1;
+
+?>
