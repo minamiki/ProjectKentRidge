c2926adf7474d1b5681fe79da2633ab12c6a0089
Yit Chun <me@minamiki.com>
2011-01-11 22:32:41 +0800

    removed old Kuizzroo files

--- webroot/returnUploadedImages.php
+++
@@ -1,4 +0,0 @@
-<?php // return uploaded images
-foreach(glob("quiz_images/".$_GET['unikey']."*") as $filename){ ?>
-<img name="" src="quiz_images/imgcrop.php?w=90&h=60&f=<?php echo basename($filename); ?>" width="90" height="60">
-<?php } ?>
