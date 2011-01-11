a7a2f2c808d51d5df127e0230c056f16fbff4e83
Yit Chun <me@minamiki.com>
2011-01-12 03:40:59 +0800

    modified resultEngine.php variables.php for the new member class

--- modules/variables.php
+++ modules/variables.php
@@ -1,6 +1,6 @@
 <?php
 //----------------------------------------
-// For global variables
+// For game variables
 //----------------------------------------
 
 // The base score for awarding of points
@@ -8,5 +8,21 @@ $BASE_POINT = 10;
 
 // The multiplier amount for each increment
 $MULTIPLIER = 1;
+
+
+//----------------------------------------
+// For game variables
+//----------------------------------------
+
+// Facebook App ID
+$FB_APPID = "154849761223760";
+
+// Facebook App Secret Key
+$FB_SECRET = "26cfea224822c58cd618eae900d87f69";
+
+
+//----------------------------------------
+// For debugging and testing
+//----------------------------------------
 
 ?>
