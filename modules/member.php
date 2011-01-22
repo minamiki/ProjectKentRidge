ac6fd1b1a46a43a23204710d7ab1e1e4bac186fc
Yit Chun <me@minamiki.com>
2011-01-22 18:14:51 +0800

    updated member.php to use OAuth 2.0

--- modules/member.php
+++ modules/member.php
@@ -5,12 +5,13 @@
 
 class Member{
 	// debugging mode
-	private $debug = true;
+	private $debug = false;
 	
 	// member variables
-	public $facebook;
-	public $id;
+	public $session = NULL;
+	public $id = NULL;
 	public $me = NULL;
+	public $facebook = NULL;
 	
 	//----------------------------------------
 	// Class constructer which
@@ -18,8 +19,7 @@ class Member{
 	// - Retrieves the facebook ID
 	//----------------------------------------
 	function __construct(){
-		if(!$this->debug){
-			require('../Connections/quizroo.php');	// database connections
+		if(!$this->debug){	
 			require('facebook.php');				// request for facebook ID
 			require('variables.php');				// global variables
 			
@@ -31,24 +31,28 @@ class Member{
 			));
 	
 			// get the session
-			$session = $facebook->getSession();
+			$this->session = $this->facebook->getSession();
 			
 			// Session based API call.
-			if($session){
+			if($this->session){
 				try{
-					$this->id = $facebook->getUser();
-					$this->me = $facebook->api('/me');	// populate the facebook $me object
+					$this->id = $this->facebook->getUser();
+					$this->me = $this->facebook->api('/me');	// populate the facebook $me object
 				}catch(FacebookApiException $e){
 					error_log($e);
+					echo $e;
 				}
+			}else{
+				// user should login to facebook first or ask for basic authentication if needed
+				$this->authenticate();
 			}
 		}else{
-			$me = true;
-			$this->$me = array('name'  => "Debug Test User");
+			$this->me = true;
+			$this->me = array('name' => "Debug Test User");
 			$this->id = 1;
 		}
 		
-		if($me){
+		if($this->me){
 			return true;
 		}else{
 			return false;
@@ -59,6 +63,7 @@ class Member{
 	// Register the user
 	//----------------------------------------
 	function register(){
+		require('../Connections/quizroo.php');	// database connections
 		// check if the member is already in the database
 		$queryCheck = sprintf("SELECT member_id FROM members WHERE member_id = %s", GetSQLValueString($facebook_id, "int"));
 		$getCheck = mysql_query($queryCheck, $quizroo) or die(mysql_error());
@@ -69,6 +74,26 @@ class Member{
 			$queryInsert = sprintf("INSERT INTO members(member_id) VALUES(%s)", GetSQLValueString($facebook_id, "int"));
 			mysql_query($queryInsert, $quizroo) or die(mysql_error());
 		}
+	}
+	
+	//----------------------------------------
+	// Generate the authentication query
+	//----------------------------------------
+	function authenticate($permissions = NULL){
+		// set the default permissons of not specified
+		$permissions = "publish_stream";
+		
+		// build the URL for O Auth 2.0 authentication
+		$url = $this->facebook->getLoginUrl(array(
+		  'canvas'     => 1
+		, 'fbconnect'  => 0
+		, 'display'    => 'page'
+		, 'cancel_url' => null
+		, 'req_perms'  => $permissions
+		));
+		
+		// redirect the user
+		echo '<script type="text/javascript">top.location.href = "'.$url.'";</script>';
 	}
 	
 	//----------------------------------------
