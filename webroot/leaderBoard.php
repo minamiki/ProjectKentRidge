dd6fe0b4e8c5300b787154a5968118da9c9dce6c
Yit Chun <me@minamiki.com>
2011-03-06 18:26:32 +0800

    implemented the Leader Board

---
+++ webroot/leaderBoard.php
@@ -0,0 +1,17 @@
+<?php include("inc/header-php.php"); ?>
+<!DOCTYPE html PUBLIC "-//W3C//DTD XHTML 1.0 Transitional//EN" "http://www.w3.org/TR/xhtml1/DTD/xhtml1-transitional.dtd">
+<html xmlns="http://www.w3.org/1999/xhtml">
+<head>
+<meta http-equiv="Content-Type" content="text/html; charset=utf-8" />
+<title>Quizroo Leaderboard</title>
+<?php include("inc/header-css.php");?>
+<link href="css/leaderboard.css" rel="stylesheet" type="text/css" />
+</head>
+
+<body>
+<?php include("../modules/statusbar.php");?>
+<?php include("../modules/leaderBoardDisplay.php"); ?>
+<?php include("inc/footer-js.php"); ?>
+<script type="text/javascript" src="js/Dashboard.js"></script>
+</body>
+</html>
