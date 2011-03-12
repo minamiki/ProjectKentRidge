6775565dc86d31659b0c87e04104d4258ca2504f
Minamiki <me@minamiki.com>
2011-03-13 05:01:22 +0800

    remove redundant OpenGraph tags from previewQuiz.php and takeQuiz.php

--- webroot/previewQuiz.php
+++ webroot/previewQuiz.php
@@ -30,29 +30,14 @@ if(isset($_GET['id'])){
 }
 $quiz = new Quiz($url_id);
 $quiz_state = $quiz->exists();
-if($quiz_state){
 ?><!DOCTYPE html PUBLIC "-//W3C//DTD XHTML 1.0 Transitional//EN" "http://www.w3.org/TR/xhtml1/DTD/xhtml1-transitional.dtd">
 <html xmlns="http://www.w3.org/1999/xhtml">
 <head>
+<meta http-equiv="Content-Type" content="text/html; charset=utf-8" />
+<?php if($quiz_state){ ?>
 <title>Quizroo: Preview Quiz</title>
-<meta property="og:title" content="<?php echo $quiz->quiz_name; ?>"/>
-<meta property="og:type" content="game"/>
-<meta property="og:image" content="<?php echo $VAR_URL."quiz_images/imgcrop.php?w=50&amp;h=50&amp;f=".$quiz->quiz_picture; ?>"/> 
-<meta property="og:url" content="<?php echo $FB_CANVAS."previewQuiz.php?id=".$quiz->quiz_id; ?>"/>
-<meta property="og:site_name" content="Quizroo"/>
-<meta property="fb:app_id" content="<?php echo $FB_APPID; ?>"/> 
-<meta http-equiv="Content-Type" content="text/html; charset=utf-8" />
-<?php }else{ ?><!DOCTYPE html PUBLIC "-//W3C//DTD XHTML 1.0 Transitional//EN" "http://www.w3.org/TR/xhtml1/DTD/xhtml1-transitional.dtd">
-<html xmlns="http://www.w3.org/1999/xhtml">
-<head>
-<meta property="og:title" content="Quizroo Quiz"/> 
-<meta property="og:type" content="game"/>
-<meta property="og:image" content=""/> 
-<meta property="og:url" content=""/>
-<meta property="og:site_name" content="Quizroo"/>
-<meta property="fb:app_id" content="<?php echo $FB_APPID; ?>"/> 
-<meta http-equiv="Content-Type" content="text/html; charset=utf-8" />
-<title>Quizroo Quiz</title>
+<?php }else{ ?>
+<title>Quizroo: Quiz not found</title>
 <?php } ?>
 <?php include("inc/header-css.php");?>
 <link href="css/quiz.css" rel="stylesheet" type="text/css" />
