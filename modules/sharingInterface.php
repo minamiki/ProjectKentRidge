0148baface85693f24ad4a1d006c8dba90df4232
Minamiki <me@minamiki.com>
2011-03-19 20:43:20 +0800

    updated sharingInterface.php with both XFBML and iframe implementations of like incase Facebook API changes

--- modules/sharingInterface.php
+++ modules/sharingInterface.php
@@ -1,23 +1,27 @@
 <?php include("variables.php"); ?>
 <div id='user-actions-container' class='frame rounded clear'>
-	<div class='share-like'>
+	<script src="http://connect.facebook.net/en_US/all.js#xfbml=1"></script>
+    <div class='share-like'>
+        <!-- XFBML -->
         <fb:like href="http://quizroo.nus-hci.com/webroot/quiz.php?id=<?php echo $quiz->quiz_id; ?>" show_faces="false" width="450"></fb:like>
+        <!-- iframe
+        <iframe src="http://www.facebook.com/plugins/like.php?href=http%3A%2F%2Fquizroo.nus-hci.com%2Fwebroot%2Fquiz.php%3Fid%3D<?php echo $quiz->quiz_id; ?>&amp;layout=standard&amp;show_faces=false&amp;width=450&amp;action=like&amp;font&amp;colorscheme=light&amp;height=35" scrolling="no" frameborder="0" style="border:none; overflow:hidden; width:450px; height:35px;" allowTransparency="true"></iframe>
+        -->
 	</div>
-    <script src="http://connect.facebook.net/en_US/all.js#xfbml=1"></script>
-		<script type="text/javascript">
-        /*
-         * Subscribe to Facebook Like event to handle it for our own data. 
-         */
-        FB.Event.subscribe('edge.create', function(response) {
-            Share.rate($('#user-actions-container'),{'quiz_id': <?php echo $quiz->quiz_id ?>,'type':1});
-        });
-        
-        /*
-         * Subscribe to Facebook Unlike event to handle it for our own data. 
-         */
-        FB.Event.subscribe('edge.remove', function(response) {
-            Share.rate($('#user-actions-container'),{'quiz_id': <?php echo $quiz->quiz_id ?>,'type':-1});
-        });
+	<script type="text/javascript">
+    /*
+     * Subscribe to Facebook Like event to handle it for our own data. 
+     */
+    FB.Event.subscribe('edge.create', function(response) {
+        Share.rate($('#user-actions-container'),{'quiz_id': <?php echo $quiz->quiz_id ?>,'type':1});
+    });
+    
+    /*
+     * Subscribe to Facebook Unlike event to handle it for our own data. 
+     */
+    FB.Event.subscribe('edge.remove', function(response) {
+        Share.rate($('#user-actions-container'),{'quiz_id': <?php echo $quiz->quiz_id ?>,'type':-1});
+    });
 	</script>
 	<div class='recommend-dialog'>
 		<fb:serverFbml width="625px">
