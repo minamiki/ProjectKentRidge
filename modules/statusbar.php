1494cfb1d2783a4b86904d653932f5b23592cbf7
Minamiki <me@minamiki.com>
2011-03-29 14:52:17 +0800

    preload score values in PHP

--- modules/statusbar.php
+++ modules/statusbar.php
@@ -25,17 +25,20 @@ if(isset($_GET['topic'])){
       <div id="statusbar-scores" class="statusbar-element">
         <div id="statusbar-quizcreator">
           <div id="statusbar-quizcreator-logo" title="Quiz Creator"></div>
-          <div id="statusbar-quizcreator-count-total" class="statusbar-achievements-quizcount-top" title="Quiz Creator Popularity Score"></div>
-          <div id="statusbar-quizcreator-count-today" class="statusbar-achievements-quizcount-bottom" title="Quiz Creator Score for Today"></div>
+          <div id="statusbar-quizcreator-count-total" class="statusbar-achievements-quizcount-top" title="Quiz Creator Popularity Score"><div class="stretch--resizer" style="margin: 0pt; padding: 0pt; white-space: nowrap; overflow: hidden; font-size: 13px; word-spacing: 0px;" type="statusbar"><span class="stretch--handle" style="margin: 0pt; padding: 0pt;" type="statusbar"><?php echo $member->quizcreator_score; ?></span></div></div>
+          <div id="statusbar-quizcreator-count-today" class="statusbar-achievements-quizcount-bottom" title="Quiz Creator Score for Today"><div class="stretch--resizer" style="margin: 0pt; padding: 0pt; white-space: nowrap; overflow: hidden; font-size: 13px; word-spacing: 0px;" type="statusbar"><span class="stretch--handle" style="margin: 0pt; padding: 0pt;" type="statusbar"><?php echo $member->quizcreator_score_today; ?></span></div>
+          </div>
         </div>
         <div id="statusbar-quiztaker">
           <div id="statusbar-quiztaker-logo" title="Quiz Taker"></div>
-          <div id="statusbar-quiztaker-count-total" class="statusbar-achievements-quizcount-top" title="Quiz Taker Score"></div>
-          <div id="statusbar-quiztaker-count-today" class="statusbar-achievements-quizcount-bottom"  title="Quiz Taker Score for Today"></div>
+          <div id="statusbar-quiztaker-count-total" class="statusbar-achievements-quizcount-top" title="Quiz Taker Score"><div class="stretch--resizer" style="margin: 0pt; padding: 0pt; white-space: nowrap; overflow: hidden; font-size: 13px; word-spacing: 0px;"><span class="stretch--handle" style="margin: 0pt; padding: 0pt;"><?php echo $member->quiztaker_score; ?></span></div></div>
+          <div id="statusbar-quiztaker-count-today" class="statusbar-achievements-quizcount-bottom"  title="Quiz Taker Score for Today"><div class="stretch--resizer" style="margin: 0pt; padding: 0pt; white-space: nowrap; overflow: hidden; font-size: 13px; word-spacing: 0px;" type="statusbar"><span class="stretch--handle" style="margin: 0pt; padding: 0pt;" type="statusbar"><?php echo $member->quiztaker_score_today; ?></span></div>
+          </div>
         </div>
       </div>
       <div id="statusbar-achievements-logo" class="statusbar-element" title="Achievements">
-        <div id="statusbar-achievements-count"></div>
+        <div id="statusbar-achievements-count"><div class="stretch--resizer" style="margin: 0pt; padding: 0pt; white-space: nowrap; overflow: hidden; font-size: 17px; word-spacing: 0px;" type="statusbar"><span class="stretch--handle" style="margin: 0pt; padding: 0pt;" type="statusbar"><?php echo $member->getStats('achievements'); ?></span></div>
+        </div>
       </div>
     </div>
     <div id="statusbar-divider"></div>
