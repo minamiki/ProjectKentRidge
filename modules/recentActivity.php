5fa386276e418f62a51c963fff4d4897d0334c49
thomato <wansong@gmail.com>
2011-04-13 22:44:42 +0800

    Fixed error when fetching friends' activities in recentActivity.php.

--- modules/recentActivity.php
+++ modules/recentActivity.php
@@ -39,32 +39,6 @@ if($totalRows_user_results != 0) { do {
 }
 
 /*
- * Get all results of quizzes of friends taken within a 1 week interval (to limit results returned)
- */
-$friends = $member->getFriendsArray();
-// If user has at least 1 friend
-if(count($friends)!=0){
-	//format list of friends for SQL e.g.: (friend_id1,friend_id2)
-	$friends = "(".implode(",",$friends).")";
-	$query_all_results = sprintf("SELECT quiz_id, quiz_name, result_title, member_id, member_name, timestamp FROM q_store_result,q_quizzes,q_results,s_members WHERE q_store_result.fk_quiz_id=q_quizzes.quiz_id AND fk_result_id=result_id AND q_store_result.fk_member_id = member_id AND timestamp > DATE_SUB(NOW(), INTERVAL 1 MONTH) AND q_store_result.fk_member_id IN %s ORDER BY q_store_result.timestamp DESC",$friends);
-	$all_results = mysql_query($query_all_results, $quizroo) or die(mysql_error());
-	$row_all_results = mysql_fetch_assoc($all_results);
-	$totalRows_all_results = mysql_num_rows($all_results);
-	
-	/*
-	 * Add all friends results to recent activities array
-	 */
-	$friends_results = 0;
-	$row_count = 0;
-	
-	if($totalRows_all_results != 0) { do {
-		$recent_activities[] = array('information'=>$row_all_results,'timestamp'=>(int) date('U',$timestamp),'type'=>0);
-		$friends_results++;
-	}while ($friends_results < $NUM_RECENT_SAMPLES && ($row_all_results = mysql_fetch_assoc($all_results)));	
-	}
-}
-
-/*
  * Get achievements user received
  */
 $query_user_achievements = sprintf("SELECT name, description, type, timestamp FROM g_achievements_log, g_achievements WHERE g_achievements_log.fk_achievement_id=g_achievements.id AND fk_member_id = %s ORDER BY g_achievements_log.timestamp DESC LIMIT %d", $facebookID, $NUM_RECENT_SAMPLES);
@@ -81,28 +55,54 @@ if($totalRows_user_achievements != 0) { do {
 } while (($row_user_achievements = mysql_fetch_assoc($user_achievements)));	
 }
 
-/*
- * Get all achievements within a 1 week interval (to limit results returned)
- */
-$query_all_achievements = sprintf("SELECT name, description, member_id, member_name, timestamp FROM g_achievements_log, g_achievements,s_members WHERE g_achievements_log.fk_achievement_id=g_achievements.id AND type<>3 AND g_achievements_log.fk_member_id = member_id AND timestamp > DATE_SUB(NOW(), INTERVAL 1 MONTH) ORDER BY g_achievements_log.timestamp DESC");
-$all_achievements = mysql_query($query_all_achievements, $quizroo) or die(mysql_error());
-$row_all_achievements = mysql_fetch_assoc($all_achievements);
-$totalRows_all_achievements = mysql_num_rows($all_achievements);
 
-/*
- * Check if achievement belong to user's friends.
- * If it does, add to recent activities array
- */
-$friends_achievements = 0;
-$row_count = 0;
+$friends = $member->getFriendsArray();
+// If user has at least 1 friend
+if(count($friends)!=0){
+	//format list of friends for SQL e.g.: (friend_id1,friend_id2)
+	$friends = "(".implode(",",$friends).")";
+	
+	/*
+ 	 * Get all results of quizzes of friends taken within a 1 week interval (to limit results returned)
+ 	 */
+	$query_all_results = sprintf("SELECT quiz_id, quiz_name, result_title, member_id, member_name, timestamp FROM q_store_result,q_quizzes,q_results,s_members WHERE q_store_result.fk_quiz_id=q_quizzes.quiz_id AND fk_result_id=result_id AND q_store_result.fk_member_id = member_id AND timestamp > DATE_SUB(NOW(), INTERVAL 1 MONTH) AND q_store_result.fk_member_id IN %s ORDER BY q_store_result.timestamp DESC",$friends);
+	$all_results = mysql_query($query_all_results, $quizroo) or die(mysql_error());
+	$row_all_results = mysql_fetch_assoc($all_results);
+	$totalRows_all_results = mysql_num_rows($all_results);
+	
+	/*
+	 * Add all friends results to recent activities array
+	 */
+	$friends_results = 0;
+	$row_count = 0;
+	
+	if($totalRows_all_results != 0) { do {
+		$timestamp = strtotime($row_all_results['timestamp']);
+		$recent_activities[] = array('information'=>$row_all_results,'timestamp'=>(int) date('U',$timestamp),'type'=>0);
+		$friends_results++;
+	}while ($friends_results < $NUM_RECENT_SAMPLES && ($row_all_results = mysql_fetch_assoc($all_results)));	
+	}
 
-if($totalRows_all_achievements != 0) { do {
-	if($member->isFriend($row_all_achievements['member_id'])){
+	/*
+	 * Get all achievements within a 1 week interval (to limit results returned)
+	 */
+	$query_all_achievements = sprintf("SELECT name, description, member_id, member_name, timestamp FROM g_achievements_log, g_achievements,s_members WHERE g_achievements_log.fk_achievement_id=g_achievements.id AND type<>3 AND g_achievements_log.fk_member_id = member_id AND timestamp > DATE_SUB(NOW(), INTERVAL 1 MONTH) AND g_achievements_log.fk_member_id IN %s ORDER BY g_achievements_log.timestamp DESC",$friends);
+	$all_achievements = mysql_query($query_all_achievements, $quizroo) or die(mysql_error());
+	$row_all_achievements = mysql_fetch_assoc($all_achievements);
+	$totalRows_all_achievements = mysql_num_rows($all_achievements);
+	
+	/*
+	 * Add all friends achievements to recent activities array
+	 */
+	$friends_achievements = 0;
+	$row_count = 0;
+	
+	if($totalRows_all_achievements != 0) { do {
 		$timestamp = strtotime($row_all_achievements['timestamp']);
 		$recent_activities[] = array('information'=>$row_all_achievements,'timestamp'=>(int) date('U',$timestamp),'type'=>1);
 		$friends_achievements++;
+	}while ($friends_achievements < $NUM_RECENT_SAMPLES && ($row_all_achievements = mysql_fetch_assoc($all_achievements)));	
 	}
-}while ($friends_achievements < $NUM_RECENT_SAMPLES && ($row_all_achievements = mysql_fetch_assoc($all_achievements)));	
 }
 
 /*
