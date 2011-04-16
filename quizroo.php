2edc8ffc165aa6516f0585203acceceff77aab29
Minamiki <me@minamiki.com>
2011-04-17 03:44:04 +0800

    added offline facebook iframe simulator

---
+++ quizroo.php
@@ -0,0 +1,52 @@
+<!DOCTYPE html PUBLIC "-//W3C//DTD XHTML 1.0 Transitional//EN" "http://www.w3.org/TR/xhtml1/DTD/xhtml1-transitional.dtd">
+<html xmlns="http://www.w3.org/1999/xhtml">
+<head>
+<meta http-equiv="Content-Type" content="text/html; charset=utf-8" />
+<title>Quizroo on Facebook</title>
+<link rel="shortcut icon" href="favicon.ico" />
+<style type="text/css">
+html, body {
+	margin: 0px;
+	padding: 0px;
+}
+#navi-bar {
+	background-image: url(fb_frame/notification_bar.png);
+	height: 41px;
+	background-repeat: no-repeat;
+	background-position: center bottom;
+	background-color: #3B5998;
+}
+#contentContainer {
+	width: 1020px;
+	margin-right: auto;
+	margin-left: auto;
+	margin-top: 20px;
+}
+#contentContainer #contentFrame {
+	display: block;
+	height: 1000px;
+	width: 760px;
+}
+#contentContainer #advert {
+	background-image: url(fb_frame/ads.png);
+	background-repeat: no-repeat;
+	background-position: -5px top;
+	float: right;
+	width: 255px;
+	height: 600px;
+	display: block;
+	margin-top: -20px;
+}
+</style>
+</head>
+
+<body>
+<div id="navi-bar"></div>
+<div id="contentContainer">
+  <div id="advert"></div>
+  <div id="contentFrame">
+    <iframe src="index.php" width="760" height="1000" scrolling="no" frameborder="0"></iframe>
+  </div>
+</div>
+</body>
+</html>
